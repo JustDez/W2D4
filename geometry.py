39fc623b7e63369b65cd41e4b535b8799672f5a4
Dev-Dez <134801129+Dev-Dez@users.noreply.github.com>
2023-11-05 21:39:36 -0600

    commit

---
+++ geometry.py
@@ -0,0 +1,25 @@
+def calculate_square_footage(length, width):
+    """
+    Calculate the square footage of a house.
+
+    Args:
+        length (float): The length of the house.
+        width (float): The width of the house.
+
+    Returns:
+        float: The square footage of the house.
+    """
+    return length * width
+
+def calculate_circumference(radius):
+    """
+    Calculate the circumference of a circle.
+
+    Args:
+        radius (float): The radius of the circle.
+
+    Returns:
+        float: The circumference of the circle.
+    """
+    import math
+    return 2 * math.pi * radius
